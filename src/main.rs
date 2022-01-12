553383c14db7cb1ed23796c994bbd6df57f4f33b
Andrei Kazakov <ay.kazakov89@gmail.com>
2022-01-12 17:14:32 +0100

    Remove success check

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,10 @@ fn main() {
         .output()
         .unwrap();
 
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout).unwrap();
-        let std_err = std::str::from_utf8(&output.stderr).unwrap();
+    let std_out = std::str::from_utf8(&output.stdout).unwrap();
+    let std_err = std::str::from_utf8(&output.stderr).unwrap();
 
-        eprint!("{}", std_err);
-        print!("{}", std_out);
-        std::process::exit(output.status.code().unwrap());
-    } else {
-        std::process::exit(1);
-    }
+    eprint!("{}", std_err);
+    print!("{}", std_out);
+    std::process::exit(output.status.code().unwrap());
 }
