5e0f0726da76685b65bcc587cf736ea4c3462edb
Andrei Kazakov <ay.kazakov89@gmail.com>
2022-04-01 09:06:34 +0200

    Make chroot work this time's for real

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::fs;
 fn main() {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
+    let command_args = &args[4..];
     let tmp = env::temp_dir().join("docker-rust-root");
     let cpath = CString::new(tmp.as_os_str().to_str().unwrap()).unwrap();
 
@@ -22,16 +23,13 @@ fn main() {
     fs::create_dir_all(tmp.join(&command_target).parent().unwrap()).unwrap();
     fs::copy(command, tmp.join(&command_target)).unwrap();
 
-
     unsafe {
         libc::chroot(cpath.as_ptr());
-        let root = CString::new("/").unwrap();
-        libc::chdir(root.as_ptr());
+        env::set_current_dir("/").unwrap();
     }
 
-    let args: &[&str] = &["adsf"];
-    let output = std::process::Command::new("echo")
-        .args(args)
+    let output = std::process::Command::new(command)
+        .args(command_args)
         .output()
         .unwrap();
     let std_out = std::str::from_utf8(&output.stdout).unwrap();
@@ -39,6 +37,5 @@ fn main() {
 
     eprint!("{}", std_err);
     print!("{}", std_out);
-    fs::remove_dir_all(&tmp).unwrap();
     std::process::exit(output.status.code().unwrap());
 }
